7b043ce2b07eaf0e3d96da4e5a93971772c0c2eb
Adelina <misssluv@yandex.ru>
2022-07-12 21:38:08 +0300

    add softAsserts

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,24 +1,32 @@
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 import runner.BaseTest;
 
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws InterruptedException {
 
            /* Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/logout']")).isEnabled());
             getDriver().findElement(By.xpath("//a[@href='/logout']")).click();
 */
+        SoftAssert softAsserts = new SoftAssert();
+
+
         getDriver().get("http://localhost:8080");
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(),  "Welcome to Jenkins!");
+        Thread.sleep(100000);
+        softAsserts.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(),  "Welcome to Jenkins!!!");
 
         getDriver().findElement(By.name("j_username")).sendKeys("test");
         getDriver().findElement(By.name("j_password")).sendKeys("test");
         getDriver().findElement(By.name("Submit")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/user/test']")).isEnabled());
+        softAsserts.assertTrue(getDriver().findElement(By.xpath("//a[@href='/user/test']")).isEnabled());
+        softAsserts.assertAll();
+
     }
 
         /*@Test
