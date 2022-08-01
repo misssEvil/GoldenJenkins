b316b05bce149be2a8bc0301df0e650f61094bbf
Adelina <misssluv@yandex.ru>
2022-08-01 18:51:05 +0300

    add @before and @after

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,70 +9,14 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void loginTest(){
-
-           /* Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/logout']")).isEnabled());
-            getDriver().findElement(By.xpath("//a[@href='/logout']")).click();
-*/
-        SoftAssert softAsserts = new SoftAssert();
-
-        getDriver().get("http://localhost:8080");
-        softAsserts.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(),  "Welcome to Jenkins!", "welcome message did not appear");
-        softAsserts.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/login?from=%2F");
-
-        //softAsserts.assertTrue(getDriver().findElement(By.xpath("//div/input[@name='Submit']")).isEnabled(), "OK button isn't present");
-        softAsserts.assertTrue(getDriver().findElement(By.name("j_username")).isEnabled(), "username field isn't present");
-        softAsserts.assertTrue(getDriver().findElement(By.name("j_password")).isEnabled(), "password field isn't present");
-        softAsserts.assertAll();
-        getDriver().findElement(By.name("j_username")).sendKeys("test");
-        getDriver().findElement(By.name("j_password")).sendKeys("test");
-        getDriver().findElement(By.name("Submit")).click();
-
-        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/user/test']")).isEnabled());
-
+        Assert.assertTrue(getDriver().findElement(By.id("jenkins-name-icon")).isEnabled());
 
     }
 
     @Test
     public void loginTest2(){
 
-           /* Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/logout']")).isEnabled());
-            getDriver().findElement(By.xpath("//a[@href='/logout']")).click();
-*/
-        SoftAssert softAsserts = new SoftAssert();
-
-        getDriver().get("http://localhost:8080");
-        softAsserts.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(),  "Welcome to Jenkins!", "welcome message did not appear");
-        softAsserts.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/login?from=%2F");
-
-        //softAsserts.assertTrue(getDriver().findElement(By.xpath("//div/input[@name='Submit']")).isEnabled(), "OK button isn't present");
-        softAsserts.assertTrue(getDriver().findElement(By.name("j_username")).isEnabled(), "username field isn't present");
-        softAsserts.assertTrue(getDriver().findElement(By.name("j_password")).isEnabled(), "password field isn't present");
-        softAsserts.assertAll();
-        getDriver().findElement(By.name("j_username")).sendKeys("oscar");
-        getDriver().findElement(By.name("j_password")).sendKeys("00000000");
-        getDriver().findElement(By.name("Submit")).click();
-
-        Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/user/oscar']")).isEnabled());
 
 
     }
-
-        /*@Test
-        public void logoWelcomeLPTest(){
-            getWait20();
-            Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/logout']")).isEnabled());
-            getDriver().findElement(By.xpath("//a[@href='/logout']")).click();
-
-            getDriver().get(String.format("http://localhost:%s", PropertyUtils.getProperties().getProperty("default.port")));
-            Assert.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(),  "Welcome to Jenkins!");
-            Assert.assertTrue(getDriver().findElement(By.xpath("//div[@class='logo']")).isDisplayed());
-
-            getDriver().findElement(By.name("j_username")).sendKeys("test");
-            getDriver().findElement(By.name("j_password")).sendKeys("test");
-            getDriver().findElement(By.name("Submit")).click();
-
-            Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href='/user/test']")).isEnabled());
-        }
-*/
-
 }
